be02da75c4db125fbb03f3ff7f8ebb4ea9ef5c91
hjm7091 <hjm7091@naver.com>
2020-03-31 21:44:21 +0900

    2장까지 완료

---
+++ src/test/java/org/project/springboot/boot/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,19 @@
+package org.project.springboot.boot.web.dto;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void lombok_functional_test() {
+        String name = "test";
+        int amount = 1000;
+
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
